2c1edbbb1084d3a8cd59a40d51133906e114b785
WilsonSanFullStack <clinicasystemlab@gmail.com>
2024-01-04 06:52:07 -0500

    todo listo

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from "./App.jsx";
 // import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./Redux/store";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
